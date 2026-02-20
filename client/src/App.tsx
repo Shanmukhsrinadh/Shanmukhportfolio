78f8e437bcd89bfe9337d8d9ba85fdcdab81f3d9
Shanmukhsrinadh <shimachalamshanmuk60297@gmail.com>
2026-02-21 04:02:59 +0530

    deployment issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,12 +6,16 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
+import { Switch, Route, Router as WouterRouter } from "wouter";
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/Shanmukhportfolio">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
